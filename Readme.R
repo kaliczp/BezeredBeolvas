9abc364d6f32320c97d17bafc999b030fb3bbe57
Péter Kalicz <kaliczp@gmail.com>
2017-10-20 19:47:46 +0200

    Felkészülés több csatornás műszerhez
    fname attributum lecserélése file-re. Talán így érthetőbb.
    
    Mivel többféle mappában van egyszerre többféle szenzor ehhez alkalmazkodva
    a fájlba összeszedett neveket (beolvhoz.txt) beolvasva, abból kiszűrhetők
    az egyes mérőhelyeg greppel.
    
    A több csatornás műszerek kezeléséhez megkerestem a másik függvényt
    (smartbe). A folyamatos támop bükkháti adataihoz kellett csináljam.
    Ezt kicsit átdolgozva működik a beolvasás.

--- Readme.R
+++ Readme.R
@@ -1,9 +1,9 @@
-beolvaso.zoo <- function(datecode,placecode="818/Bezered1_",object=NULL){
+## Kiindulás Városi adatok Readme.R
+beolvaso.zoo <- function(file, channel=1, object=NULL){
     require(zoo)
     if(is.null(object)){
-        fname <- paste0(placecode,datecode[1],".TXT")
-        rec <- read.table(fname, sep="\t",skip=12,dec=".",fill=TRUE,na=c("--------------------------------------------------------"))
-        print(fname)
+        rec <- read.table(file, sep="\t",skip=12,dec=".",fill=TRUE,na=c("--------------------------------------------------------"))
+        print(file)
         out <- zoo(rec[-nrow(rec),7],strptime(as.character(rec[-nrow(rec),2]),"%Y.%m.%d %T"))
         datecode <- datecode[-1]
     } else {
@@ -11,8 +11,7 @@ beolvaso.zoo <- function(datecode,placecode="818/Bezered1_",object=NULL){
     }
     ## Zooba beolvasó fő ciklus. Létező objektum esetén ez dolgozik.
     for(current.datecode in datecode){
-        fname <- paste(placecode,current.datecode,".txt",sep="")
-        rec <- read.table(fname, sep="\t",skip=12,dec=".",fill=TRUE,na=c("--------------------------------------------------------"))
+        rec <- read.table(file, sep="\t",skip=12,dec=".",fill=TRUE,na=c("--------------------------------------------------------"))
         print(fname)
         rec.zoo <- zoo(rec[-nrow(rec),7],strptime(as.character(rec[-nrow(rec),2]),"%Y.%m.%d %T"))
     out <- c(out,rec.zoo)
@@ -20,10 +19,36 @@ beolvaso.zoo <- function(datecode,placecode="818/Bezered1_",object=NULL){
     out
 }
 
+## Folyamatostámop zárójelentés Bükkhát alapján
+smartbe <- function(file, channel=1) {
+    headsep <- which(readLines(file) == "========================================================")
+    channelsep <- which(readLines(file) == "--------------------------------------------------------")
+    head.length <- 8
+    if(channel == 1){
+        ch.start <- headsep[1]+head.length
+        ch.end <- channelsep[1]-(headsep[1]+head.length+1)
+    } else {
+        ch.start <- headsep+head.length
+        ch.end <- channelsep[1]-(headsep+head.length+1)
+    }
+    data <- scan(file, what=list(NULL,"",NULL,NULL,NULL,NULL,numeric()),
+                 skip= ch.start , nlines=ch.end, sep="\t",
+                 fill=T,fileEncoding="latin1", na.string="?")
+    data.frame(DateTime = data[[2]], Measure = data[[7]])
+}
+
+beolvhoz <- scan("beolvhoz.txt", character())
+
 ######################################################################
 ## Beolvasás
 ######################################################################
-Bezered1 <- beolvaso.zoo(818,"818/Bezered1_")
-Bezered2 <- beolvaso.zoo(818,"818/Bezered2_")
-Bezered3 <- beolvaso.zoo(818,"818/Bezered4_")
-Bezered4 <- b4818[-length(b4818)]
+grep("Bezered1", beolvhoz)
+grep("Bezered2", beolvhoz)
+grep("Bezered3", beolvhoz)
+grep("Bezered4", beolvhoz)
+grep("Bezered5", beolvhoz)
+Bezered1 <- smartbe(beolvhoz[1])
+Bezered2 <- smartbe(beolvhoz[2])
+Bezered3 <- smartbe(beolvhoz[9])
+Bezered4 <- smartbe(beolvhoz[3])
+Bezered5 <- smartbe(beolvhoz[15])
